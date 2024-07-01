39ee3adb27001ec676767144fb310f52c77487bc
theissenhelen <helen.theissen@ecmwf.int>
2024-07-01 14:03:03 +0000

    fix: imports

--- src/anemoi/graphs/nodes/__init__.py
+++ src/anemoi/graphs/nodes/__init__.py
@@ -1,4 +1,6 @@
+from .builder import HexRefinedIcosahedralNodes
 from .builder import NPZFileNodes
+from .builder import TriRefinedIcosahedralNodes
 from .builder import ZarrDatasetNodes
 
-__all__ = ["ZarrDatasetNodes", "NPZFileNodes"]
+__all__ = ["ZarrDatasetNodes", "NPZFileNodes", "TriRefinedIcosahedralNodes", "HexRefinedIcosahedralNodes"]
