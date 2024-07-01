e410bf58c997686d517698cf00f0424545e33c9e
theissenhelen <helen.theissen@ecmwf.int>
2024-07-01 16:50:01 +0000

    fix: imports

--- src/anemoi/graphs/edges/__init__.py
+++ src/anemoi/graphs/edges/__init__.py
@@ -1,4 +1,6 @@
 from .builder import CutOffEdges
+from .builder import HexagonalEdges
 from .builder import KNNEdges
+from .builder import TriIcosahedralEdges
 
-__all__ = ["KNNEdges", "CutOffEdges"]
+__all__ = ["KNNEdges", "CutOffEdges", "TriIcosahedralEdges", "HexagonalEdges"]
