19c38135f5cd9665cee6158cce322317e352916e
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2024-08-08 13:35:17 +0000

    fix: cli config arg casted to Path

--- src/anemoi/graphs/commands/create.py
+++ src/anemoi/graphs/commands/create.py
@@ -18,7 +18,7 @@ class Create(Command):
             help="Overwrite existing files. This will delete the target graph if it already exists.",
         )
         command_parser.add_argument(
-            "config", help="Configuration yaml file path defining the recipe to create the graph."
+            "config", type=Path, help="Configuration yaml file path defining the recipe to create the graph."
         )
         command_parser.add_argument("save_path", type=Path, help="Path to store the created graph.")
 
