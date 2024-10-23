e29ca1034507d31d69010c6e856b93e0d90aa5b2
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2024-10-23 12:01:30 +0000

    fix: tests

--- tests/nodes/test_cutout_nodes.py
+++ tests/nodes/test_cutout_nodes.py
@@ -4,7 +4,6 @@ from omegaconf import OmegaConf
 from torch_geometric.data import HeteroData
 
 from anemoi.graphs.nodes.attributes import AreaWeights
-from anemoi.graphs.nodes.attributes import CutOutMask
 from anemoi.graphs.nodes.attributes import UniformWeights
 from anemoi.graphs.nodes.builders import from_file
 
@@ -12,7 +11,9 @@ from anemoi.graphs.nodes.builders import from_file
 def test_init(mocker, mock_zarr_dataset_cutout):
     """Test ZarrDatasetNodes initialization with cutout."""
     mocker.patch.object(from_file, "open_dataset", return_value=mock_zarr_dataset_cutout)
-    node_builder = from_file.ZarrDatasetNodes(forcing_dataset="global.zarr", lam_dataset="lam.zarr", name="test_nodes")
+    node_builder = from_file.ZarrDatasetNodes(
+        OmegaConf.create({"cutout": ["lam.zarr", "global.zarr"]}), name="test_nodes"
+    )
 
     assert isinstance(node_builder, from_file.BaseNodeBuilder)
     assert isinstance(node_builder, from_file.ZarrDatasetNodes)
@@ -22,7 +23,7 @@ def test_register_nodes(mocker, mock_zarr_dataset_cutout):
     """Test ZarrDatasetNodes register correctly the nodes with cutout operation."""
     mocker.patch.object(from_file, "open_dataset", return_value=mock_zarr_dataset_cutout)
     node_builder = from_file.ZarrDatasetNodes(
-        OmegaConf.create({"cutout": ["global.zarr", "lam.zarr"]}), name="test_nodes"
+        OmegaConf.create({"cutout": ["lam.zarr", "global.zarr"]}), name="test_nodes"
     )
     graph = HeteroData()
 
@@ -34,12 +35,12 @@ def test_register_nodes(mocker, mock_zarr_dataset_cutout):
     assert graph["test_nodes"].node_type == "ZarrDatasetNodes"
 
 
-@pytest.mark.parametrize("attr_class", [UniformWeights, AreaWeights, CutOutMask])
+@pytest.mark.parametrize("attr_class", [UniformWeights, AreaWeights])
 def test_register_attributes(mocker, mock_zarr_dataset_cutout, graph_with_nodes: HeteroData, attr_class):
     """Test ZarrDatasetNodes register correctly the weights with cutout operation."""
     mocker.patch.object(from_file, "open_dataset", return_value=mock_zarr_dataset_cutout)
     node_builder = from_file.ZarrDatasetNodes(
-        OmegaConf.create({"cutout": ["global.zarr", "lam.zarr"]}), name="test_nodes"
+        OmegaConf.create({"cutout": ["lam.zarr", "global.zarr"]}), name="test_nodes"
     )
     config = {"test_attr": {"_target_": f"anemoi.graphs.nodes.attributes.{attr_class.__name__}"}}
 
