7f6e4258378ae113f10bc6c3cf8d5c612e65741f
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2024-12-11 16:20:46 +0000

    fix: update tests

--- tests/edges/test_edge_attributes.py
+++ tests/edges/test_edge_attributes.py
@@ -13,26 +13,26 @@ import torch
 from anemoi.graphs.edges.attributes import EdgeDirection
 from anemoi.graphs.edges.attributes import EdgeLength
 
+TEST_EDGES = ("test_nodes", "to", "test_nodes")
 
-@pytest.mark.parametrize("norm", ["l1", "l2", "unit-max", "unit-std"])
-@pytest.mark.parametrize("luse_rotated_features", [True, False])
-def test_directional_features(graph_nodes_and_edges, norm, luse_rotated_features: bool):
+
+@pytest.mark.parametrize("norm", ["l1", "l2", "unit-max", "unit-std", "unit-range"])
+def test_directional_features(graph_nodes_and_edges, norm):
     """Test EdgeDirection compute method."""
-    edge_attr_builder = EdgeDirection(norm=norm, luse_rotated_features=luse_rotated_features)
-    edge_attr = edge_attr_builder.compute(graph_nodes_and_edges, ("test_nodes", "to", "test_nodes"))
+    edge_attr_builder = EdgeDirection(norm=norm)
+    edge_index = graph_nodes_and_edges[TEST_EDGES].edge_index
+    source_coords = graph_nodes_and_edges[TEST_EDGES[0]].x
+    target_coords = graph_nodes_and_edges[TEST_EDGES[2]].x
+    edge_attr = edge_attr_builder(x=(source_coords, target_coords), edge_index=edge_index)
     assert isinstance(edge_attr, torch.Tensor)
 
 
-@pytest.mark.parametrize("norm", ["l1", "l2", "unit-max", "unit-std"])
+@pytest.mark.parametrize("norm", ["l1", "l2", "unit-max", "unit-std", "unit-range"])
 def test_edge_lengths(graph_nodes_and_edges, norm):
     """Test EdgeLength compute method."""
     edge_attr_builder = EdgeLength(norm=norm)
-    edge_attr = edge_attr_builder.compute(graph_nodes_and_edges, ("test_nodes", "to", "test_nodes"))
+    edge_index = graph_nodes_and_edges[TEST_EDGES].edge_index
+    source_coords = graph_nodes_and_edges[TEST_EDGES[0]].x
+    target_coords = graph_nodes_and_edges[TEST_EDGES[2]].x
+    edge_attr = edge_attr_builder(x=(source_coords, target_coords), edge_index=edge_index)
     assert isinstance(edge_attr, torch.Tensor)
-
-
-@pytest.mark.parametrize("attribute_builder", [EdgeDirection(), EdgeLength()])
-def test_fail_edge_features(attribute_builder, graph_nodes_and_edges):
-    """Test edge attribute builder fails with unknown nodes."""
-    with pytest.raises(AssertionError):
-        attribute_builder.compute(graph_nodes_and_edges, ("test_nodes", "to", "unknown_nodes"))
